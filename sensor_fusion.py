1d66c2f13bd0b09a2c0fd6eb67510030ed32c9d6
Justin Burden <jburd2790@gmail.com>
2019-03-12 22:53:40 -0400

    Modified sonar fusion script to be callable by other scripts

--- sensor_fusion.py
+++ sensor_fusion.py
@@ -13,60 +13,71 @@ num_rows, num_cols = 8, 4
 def gaussian(x, mu, var):
     return np.exp(-np.power(x - mu, 2.) / (2 * var))/np.sqrt(2*np.pi*var)
 
+def calculateSensorFusion(left_distance, right_distance):
+    # first number is average, second is variance
+    # these characterize the normal distribution of distance at each square
+    left_cpt = []
+    right_cpt = []
+    fusion_cpt = []
 
-# first number is average, second is variance
-# these characterize the normal distribution of distance at each square
-left_cpt = []
-right_cpt = []
+    # load up left_cpt from its CPT CSV
+    print "loading left sonar CPTs"
+    with open('left_sonar_straight_cpt.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
 
-# load up left_cpt from its CPT CSV
-print "loading left sonar CPTs"
-with open('left_sonar_straight_cpt.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-
-    for row in csv_reader:
-        # norm is for each individual normal distribution
-        norm = map(lambda x: float(x), row[0].split(', '))
-        left_cpt.append(norm)
-        print norm
-
-
-# load up right_cpt from its CPT CSV
-print "loading right sonar CPTs"
-with open('right_sonar_straight_cpt.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-
-    for row in csv_reader:
-        # norm is for each individual normal distribution
-        norm = map(lambda x: float(x), row[0].split(', '))
-        right_cpt.append(norm)
-        print norm
+        for row in csv_reader:
+            # norm is for each individual normal distribution
+            norm = map(lambda x: float(x), row[0].split(', '))
+            left_cpt.append(norm)
+            print norm
 
 
-# sensor input currently by command line - this should be changed when implemented on the NAO. 
-print "type in left sonar distance from wall: "
-left_distance = input()
+    # load up right_cpt from its CPT CSV
+    print "loading right sonar CPTs"
+    with open('right_sonar_straight_cpt.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
 
-print "type in right sonar distance from wall: "
-right_distance = input()
-
-
-# find the effective maximum probability and its associated distance coordinate (row in the convention of this program)
-max_index = 0
-
-for row in range(num_rows):
-    print "Coordinate", row
-    print "Left probability: ", gaussian(left_distance, np.average(left_cpt[row]), np.var(left_cpt[row]))
-    print "Right probability: ", gaussian(right_distance, np.average(right_cpt[row]), np.var(right_cpt[row]))
-
-    # no need for finding the complete probability (also factoring in normalization and probability of being in the distance coordinate), so only two multiplications are needed
-    if gaussian(left_distance, np.average(left_cpt[row]), np.var(left_cpt[row]))*gaussian(right_distance, np.average(right_cpt[row]), np.var(right_cpt[row])) > gaussian(left_distance, np.average(left_cpt[max_index]), np.var(left_cpt[max_index]))*gaussian(right_distance, np.average(right_cpt[max_index]), np.var(right_cpt[max_index])):
-        max_index = row
+        for row in csv_reader:
+            # norm is for each individual normal distribution
+            norm = map(lambda x: float(x), row[0].split(', '))
+            right_cpt.append(norm)
+            print norm
 
 
-# print most likely distance coordinate (0 being the farthest at 20 inches away from the wall)
-print "most likely distance coordinate: ", max_index
-print "most likely distance coordinate left CPT: ", left_cpt[max_index]
-print "most likely distance coordinate left probability: ", gaussian(left_distance, np.average(left_cpt[max_index]), np.var(left_cpt[max_index]))
-print "most likely distance coordinate right CPT: ", right_cpt[max_index]
-print "most likely distance coordinate right probability: ",gaussian(right_distance, np.average(right_cpt[max_index]), np.var(right_cpt[max_index]))
+    # find the effective maximum probability and its associated distance coordinate (row in the convention of this program)
+    max_index = 0
+
+    for row in range(num_rows):
+        print "Coordinate", row
+        leftProb = gaussian(left_distance, np.average(left_cpt[row]), np.var(left_cpt[row]))
+        rightProb = gaussian(right_distance, np.average(right_cpt[row]), np.var(right_cpt[row]))
+        fusionProb = leftProb * rightProb
+        print "Left probability: ", leftProb
+        print "Right probability: ", rightProb
+        print "Fusion probability: ", fusionProb
+        fusion_cpt.append(fusionProb)
+
+        # no need for finding the complete probability (also factoring in normalization and probability of being in the distance coordinate), so only two multiplications are needed
+        if fusionProb > fusion_cpt[max_index]:
+            max_index = row
+
+
+    # print most likely distance coordinate (0 being the farthest at 20 inches away from the wall)
+    print "most likely distance coordinate: ", max_index
+    print "most likely distance coordinate left CPT: ", left_cpt[max_index]
+    print "most likely distance coordinate left probability: ", gaussian(left_distance, np.average(left_cpt[max_index]), np.var(left_cpt[max_index]))
+    print "most likely distance coordinate right CPT: ", right_cpt[max_index]
+    print "most likely distance coordinate right probability: ",gaussian(right_distance, np.average(right_cpt[max_index]), np.var(right_cpt[max_index]))
+
+    print "Fusion Probabilities: ", fusion_cpt
+    
+    return fusion_cpt
+
+if __name__ == '__main__':
+    print "type in left sonar distance from wall: "
+    left_distance = input()
+
+    print "type in right sonar distance from wall: "
+    right_distance = input()
+    
+    calculateSensorFusion(left_distance, right_distance)
